4674eb661c495c05ce6d0b93330342a5b598a08e
Satr11oBoyd <satriobudyo.id@gmail.com>
2025-10-14 00:17:29 +0700

    asset update

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import path from 'path';
 
 export default defineConfig({
-    base: '/public/build/',
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    build: {
-        outDir: 'public/build',
-        manifest: true,
-        emptyOutDir: true,
-    },
 });
